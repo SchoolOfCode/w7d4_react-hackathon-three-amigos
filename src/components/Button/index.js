1dc8f7ed38758f6134e2f53c18dda052380d4847
atemp678 <ajt2158@columbia.edu>
2022-01-11 16:41:55 +0000

    linting error fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
 
 function Button({ onClick }) {
-  return <button className="addButton" onClick={onClick}>Add to the List!</button>;
+  return (
+    <button className="addButton" onClick={onClick}>
+      Add to the List!
+    </button>
+  );
 }
 
 export default Button;
